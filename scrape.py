7e31aa16214d9c8bd51724a27fb09314482583e8
Brandon Browning <brandonbrowning@live.com>
2013-04-15 19:41:39 +0000

    Basic rendering to output/index.html

--- scrape.py
+++ scrape.py
@@ -1,16 +1,41 @@
 
+import re
 import urllib2
 from lxml import etree
+
+class Problem:
+	def __init__(self, problemid=None, urlid=None, name=None):
+		self.problemid = problemid
+		self.urlid = urlid # (123, B)
+		self.name = name
+
+	@property
+	def url(self):
+		return "http://codeforces.com/problemset/problem/{0}/{1}".format(self.urlid[0], self.urlid[1])
+
+	@property
+	def urlname(self):
+		return "{0}{1}".format(self.urlid[0], self.urlid[1]) if self.urlid else ""
 
 class Submission:
 	def __init__(self):
 		self.submissionid = None
 		self.date = None
-		self.problemid = None
+		self.problem = None
 		self.language = None
 		self.verdict = None
 		self.time = None # ms
 		self.memory = None # KB
+
+class StudentWeek:
+	def __init__(self, name=None, correct=[], participantid=None):
+		self.name = name
+		self.correct = correct
+		self.participantid = participantid
+
+	@property
+	def url(self):
+		return "http://codeforces.com/profile/{0}".format(self.name)
 
 def extract_string(tree, xpath_query):
 	results = tree.xpath(xpath_query)
@@ -31,6 +56,18 @@ def serialize_verdict(verdict_text):
 	else:
 		raise ValueError('Given verdict not known ({0})'.format(verdict_text))
 
+def extract_problem_urlid_and_name(problem_title):
+	match = re.search('^(\d+)(\w) - (.*)$', problem_title)
+	if not match:
+		return (None, None)
+	else:
+		groups = match.groups()
+
+		if len(groups) == 3:
+			urlid = (groups[0], groups[1])
+			name = groups[2]
+			return (urlid, name)
+
 def gen_submissions(username):
 	url = 'http://codeforces.com/submissions/{0}'.format(username)
 	response = urllib2.urlopen(url)
@@ -41,11 +78,17 @@ def gen_submissions(username):
 
 		result.submissionid = int(extract_string(submission, 'td[1]/a/text()'))
 		result.date = extract_string(submission, 'td[2]/text()')
-		result.problemid = int(extract_string(submission, 'td[4]/@data-problemid'))
 		result.language = extract_string(submission, 'td[5]/text()')
 		result.verdict = serialize_verdict(extract_string(submission, 'td[6]/span/@class'))
 		result.time = int(extract_string(submission, 'td[7]/text()').replace(' ms', ''))
 		result.memory = int(extract_string(submission, 'td[8]/text()').replace(' KB', ''))
+
+		problem = Problem()
+		problem.problemid = int(extract_string(submission, 'td[4]/@data-problemid'))
+		submission_problem_cell_text = extract_string(submission, 'td[4]/a/text()')
+		problem.urlid, problem.description = extract_problem_urlid_and_name(submission_problem_cell_text)
+
+		result.problem = problem
 
 		if not result.verdict:
 			result.verdict = serialize_verdict(extract_string(submission, 'td[6]/a/text()'))
--- test.py
+++ test.py
@@ -1,11 +1,51 @@
 
-import scrape
+from scrape import gen_submissions, Problem, StudentWeek
+from wheezy.template.engine import Engine
+from wheezy.template.ext.core import CoreExtension
+from wheezy.template.loader import FileLoader
 
-people = ['ermiar', 'DrChickenSalad']
+title = 'ACMUA - SIGCOMP - Leaderboard'
+css = ["css/bootstrap.min.css", "css/font-awesome.min.css", "css/site.css"]
+js = ["js/jquery.min.js", "http://d3js.org/d3.v3.min.js", "js/site.js"]
+winner_text = 'TODO'
 
-for person in people:
-	subs = list(scrape.gen_submissions(person))
-	correct = set([s.problemid for s in subs if s.verdict == 'Accepted'])
-	week_problems = set([1798, 969])
-	scoreboard = [(p, p in correct) for p in week_problems]
-	print('{0}: {1}'.format(person, scoreboard))
+template_path = 'template/'
+engine = Engine(loader=FileLoader([template_path]), extensions=[CoreExtension()])
+
+index_template = engine.get_template('index.html')
+
+week_problems_unsorted = [Problem(1798, ('223', 'C'), 'Partial Sums'), Problem(969, ('158', 'A'), 'Next Round')]
+week_problems = set(sorted(week_problems_unsorted, key=lambda p: p.problemid))
+
+def completed_to_icon_html(correct):
+	return "<i class=\"{0} icon-3x\"></i>".format("icon-ok" if correct else "icon-remove")
+
+def gen_correct_problem_mapping(submissions, week_problemids):
+	accepted_submissions = set([s.problem.problemid for s in submissions if s.verdict == 'Accepted'])
+	for problemid in week_problemids:
+		completed = problemid in accepted_submissions
+		yield (problemid, completed, completed_to_icon_html(completed))
+
+profile_names = ['ermiar', 'DrChickenSalad']
+def gen_people(profile_names, week_problem):
+	week_problemids = [p.problemid for p in week_problems]
+	for person in profile_names:
+		submissions = list(gen_submissions(person))
+		correct_mapping = gen_correct_problem_mapping(submissions, week_problemids)
+		student_week = StudentWeek(person, sorted(correct_mapping))
+
+		print('{0}: {1}'.format(student_week.name, student_week.correct))
+		yield student_week
+
+people = list(gen_people(profile_names, week_problems))
+render_values = {
+	'title': title,
+	'css': css,
+	'js': js,
+	'winner_text': winner_text,
+	'week_problems': week_problems,
+	'people': people
+}
+
+with open('output/index.html', 'w') as f:
+	f.write(index_template.render(render_values))
