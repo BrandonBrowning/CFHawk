234a2f525407d359a64b7641cc94c03e007cdc18
BrandonBrowning <brandonbrowning@live.com>
2016-01-28 23:41:52 -0500

    Load config from toml and updated README to actually be useful

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
+
 CFHawk
 ======
 
-Information retriever from codeforces for UA ACM SIGCOMP
+Scoreboard and information retriever from codeforces.com for UA ACM SIGCOMP
+
 
 Installation
 ============
@@ -13,3 +15,68 @@ Not fully tested, but this is the general idea
 
     sudo apt-get install python python-pip libxml2-dev libxslt1-dev python-dev
     sudo pip install -r requirements.txt
+
+
+Operation
+=========
+
+Run `./update-scoreboard.py` at whatever interval you want to update the scoreboard.
+
+Note: this should probably be no more than once a minute due to the amount of scraping
+
+
+Host the static files in the configured output directory using the webserver of your choice.
+
+If you want, you can run `python -m SimpleHTTPServer` in the directory and it should work.
+
+
+Configuration
+=============
+
+Create a file resembling the following at `config.toml`
+
+```
+[ui]
+title = "ACMUA - SIGCOMP - Scoreboard"
+
+[template]
+input_path = "template"
+output_path = "output"
+index_local_path = "index.html"
+
+[[problems]]
+id = 41188
+set = "599"
+letter = "A"
+name = "Patrick and Shopping"
+
+[[problems]]
+id = 41189
+set = "599"
+letter = "B"
+name = "Spongebob and Joke"
+
+[[problems]]
+id = 41190
+set = "599"
+letter = "C"
+name = "Day at the Beach"
+
+[[problems]]
+id = 41191
+set = "599"
+letter = "D"
+name = "Spongebob and Squares"
+
+[[people]]
+handle = "t.wynn"
+name = "Thomas Wynn"
+
+[[people]]
+handle = "amb288"
+name = "Aaron Battershell"
+
+[[people]]
+handle = "DrChickenSalad"
+name = "Brandon Browning"
+```
--- update-scoreboard.py
+++ update-scoreboard.py
@@ -1,25 +1,27 @@
+#!/usr/bin/env python2
 
 import os
 from datetime import datetime
 
-from config import *
+import toml
+from easydict import EasyDict as edict
 from scrape import gen_submissions, Problem, StudentWeek
 from wheezy.template.engine import Engine
 from wheezy.template.ext.core import CoreExtension
 from wheezy.template.loader import FileLoader
 
-title = 'ACMUA - SIGCOMP - Leaderboard'
+with open('config.toml') as f:
+    config = edict(toml.loads(f.read()))
+
+title = config.ui.title
 css = ["css/bootstrap.min.css", "css/font-awesome.min.css", "css/site.css"]
 js = ["js/jquery.min.js", "http://d3js.org/d3.v3.min.js", "js/site.js"]
-winner_text = 'TODO'
+winner_text = 'TODO Winner Text!'
 
-template_path = 'template/'
-engine = Engine(loader=FileLoader([template_path]), extensions=[CoreExtension()])
+engine = Engine(loader=FileLoader([config.template.input_path]), extensions=[CoreExtension()])
+index_template = engine.get_template(config.template.index_local_path)
 
-index_template = engine.get_template('index.html')
-
-week_problems_unsorted = [Problem(2702, ('279', 'B'), 'Books'), Problem(35996, ('580', 'B'), 'Kefa and Company'), Problem(1013, ('165', 'B'), 'Burning Midnight Oil')]
-week_problems = set(sorted(week_problems_unsorted, key=lambda p: p.problemid))
+week_problems = [Problem(p.id, (p.set, p.letter), p.name) for p in config.problems]
 
 def completed_to_icon_html(correct):
 	return "<i class=\"{0} icon-3x\"></i>".format("icon-ok" if correct else "icon-remove")
@@ -31,7 +33,7 @@ def gen_correct_problem_mapping(submissions, week_problemids):
 
 		yield (problemid, completed, completed_to_icon_html(completed))
 
-profile_names = ['ermiar', 'DrChickenSalad', 't.wynn', 'anb288']
+profile_names = [person.handle for person in config.people]
 def gen_people(profile_names, week_problem):
 	week_problemids = [p.problemid for p in week_problems]
 	for person in profile_names:
@@ -52,7 +54,7 @@ render_values = {
 }
 
 def output_file(filename):
-	return os.path.join(config_output_directory, filename)
+	return os.path.join(config.template.output_path, filename)
 
 with open(output_file('index.html'), 'w') as f:
 	f.write(index_template.render(render_values))
